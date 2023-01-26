861c1c890b2456e0df56a252d10e84f1898f53b8
새싹 <123479997+Planting12@users.noreply.github.com>
2023-01-27 02:06:16 +0900

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,22 @@
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+
+  min: function (a, b) {
+    console.log(a - b);
+  },
+
+  div: function (a, b) {
+    console.log(a / b);
+  },
+
+  powerof: function (a, b) {
+    console.log(a ** b);
+  },
+};
+
+calculator.add(20, 23);
+calculator.min(20, 23);
+calculator.div(20, 23);
+calculator.powerof(20, 23);
